26dd4a73cada7e07b3a47f73bec85cff2b646fab
Anthony Chin <canthony@ada.cs.pdx.edu>
2020-11-30 20:42:13 -0800

    updated to include array and player class

--- controls.h
+++ controls.h
@@ -1,11 +1,12 @@
-void add_energy(int & energy, int to_add);
-void remove_energy(int & energy, int to_remove);
-void add_whiffles(int & whiffles, int to_add);
-void remove_whiffles(int & whiffles, int to_remove);
-int check_next(int ch, cell map[128][128], int y, int x, int ymax, int xmax);
-void move(int ch, cell map[128][128], int & y, int & x, int ymax, int xmax, int & energy);
-void buy_food(int & whiffles, int & energy, int whiffles_remove, int energy_add);
-void buy_tool(int & whiffles);
-void remove_obstacle(int & energy, int energy_cost);
+//void add_energy(int & energy, int to_add);
+//void remove_energy(int & energy, int to_remove);
+//void add_whiffles(int & whiffles, int to_add);
+//void remove_whiffles(int & whiffles, int to_remove);
+int check_next(int ch, array & map, int y, int x, int ymax, int xmax);
+void move(int ch, array & map, int & y, int & x, int ymax, int xmax, player & hero);
+void buy_food(player & hero, array & map, int y, int x);
+void buy_tool(player & hero, array & map, int y, int x);
+void remove_obstacle(player & hero, array & map, int y, int x);
+void open_chest(player & hero, array & map, int y, int x);
 
 
