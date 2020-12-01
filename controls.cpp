85d9adb9b14add1e932f9055599f4a4f7c468ca5
Anthony Chin <canthony@ada.cs.pdx.edu>
2020-11-30 22:32:54 -0800

    edited move function to include check_check()

--- controls.cpp
+++ controls.cpp
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include "controls.h"
+using namespace std;
 
 /*void add_energy(int & energy, int to_add)
 {
@@ -25,10 +26,12 @@ void remove_whiffles(int & whiffles, int to_remove)
 	return;
 }*/
 
+//Checks to see the next tile before the player moves there
 int check_next(int ch, array & map, int y, int x, int ymax, int xmax)
 {
 
 	Tile * temp_tile = NULL;
+	
 
 	if(ch == KEY_DOWN && y < ymax)
 	{
@@ -51,10 +54,13 @@ int check_next(int ch, array & map, int y, int x, int ymax, int xmax)
 	return temp_tile.number;
 }
 
-void move(int ch, array & map, int & y, int & x, int ymax, int xmax, player & hero)
+//moves the player and performs various actions based on the cell
+void move(int ch, player & hero, array & map, int & y, int & x, int ymax, int xmax)
 {
 	int tile_check;
+	char item_check = 'a';
 
+	//checks to see what the next tile is
 	tile_check = check_next(ch, map, y, x, ymax, xmax);
 	if(tile_check == 4)
 	{
@@ -70,7 +76,7 @@ void move(int ch, array & map, int & y, int & x, int ymax, int xmax, player & he
 		hero.loseEnergy(1);
 	}
 
-
+	//moves the player to the next tile
 	if(ch == KEY_UP && y > 0)
 	{
 		--y;
@@ -87,8 +93,31 @@ void move(int ch, array & map, int & y, int & x, int ymax, int xmax, player & he
 	{
 		++x;
 	}
+
+	//checks to see what item is in a tile
+	item_check = map.check_cell(y, x);
+
+	if(item_check == FOOD)
+	{
+		buy_food(hero, map, y, x);
+	}
+	if(item_check == TOOL)
+	{
+		buy_tool(hero, map, y, x);
+	}
+	if(item_check == OBSTACLE)
+	{
+		remove_obstacle(hero, map, y, x);
+	}
+	if(item_check == CHEST)
+	{
+		open_chest(hero, map, y, x);
+	}
+
+
 }
 
+//buys food, adding energy and removing whiffles from the player
 void buy_food(player & hero, array & map, int y, int x)
 {
 	food * tile_food = map.get_food(y, x);
@@ -104,6 +133,7 @@ void buy_food(player & hero, array & map, int y, int x)
 	return;
 }
 
+//buys a tool, removing whiffles and adding a tool to the player
 void buy_tool(player & hero, array & map, int y, int x)
 {
 	tool * tile_tool = map.get_tool(y, x);
@@ -118,20 +148,23 @@ void buy_tool(player & hero, array & map, int y, int x)
 	return;
 }
 
+//removes an obstacle from the map. checks to see if the player has a matching
+//tool type and if so, uses up the tool
 void remove_obstacle(player & hero, array & map, int y, int x)
 {
 	//not finished
 	obstacle * tile_obstacle = map.get_obstacle(y, x);
 	int cost_divider = hero.retrieve(tile_obstacle.type);
+	
+	hero.loseEnergy(tile_obstacle.cost/cost_divider);
 
-	if(cost_divider == 0)
-	{
-		hero.loseEngery(tile_obstacle.cost);
+	hero.remove(tile_obstacle.name);
 
-	}
+
 	return;
 }
 
+//opens a chest, removing a chest from the map and adding whiffles to the player
 void open_chest(player & hero, array & map, int y, int x)
 {
 	chest * tile_chest = map.get_chest(y, x);
